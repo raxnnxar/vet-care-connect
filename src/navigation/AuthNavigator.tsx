5eb2f0412c164c25d9c1e71eff46f2f2ba2133b8
raxnnxar <firifologo@gmail.com>
2025-04-21 12:42:53 -0600

    Update AuthNavigator.tsx

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,36 +1,23 @@
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { SCREENS } from './navigationConfig';
+import { ROUTES } from '@/frontend/shared/constants/routes';
 import WelcomeScreen from '../features/auth/screens/WelcomeScreen';
 import SignupScreen from '../features/auth/screens/SignupScreen';
-
-// Placeholder screen components
-const LoginScreen = () => (
-  <div className="flex items-center justify-center h-screen">
-    <div className="text-center">
-      <h2 className="text-xl font-semibold">Login Screen</h2>
-    </div>
-  </div>
-);
-
-const ForgotPasswordScreen = () => (
-  <div className="flex items-center justify-center h-screen">
-    <div className="text-center">
-      <h2 className="text-xl font-semibold">Forgot Password Screen</h2>
-    </div>
-  </div>
-);
+import LoginScreen from '../features/auth/screens/LoginScreen';
+import ForgotPasswordScreen from '../features/auth/screens/ForgotPasswordScreen';
+import ResetPasswordScreen from '../features/auth/screens/ResetPasswordScreen';
 
 const AuthNavigator = () => {
   return (
     <div className="relative h-full">
       <Routes>
         <Route path="/" element={<WelcomeScreen />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/signup" element={<SignupScreen />} />
-        <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
-        {/* Post-signup routes are now handled in AppNavigator */}
+        <Route path={ROUTES.LOGIN.replace('/', '')} element={<LoginScreen />} />
+        <Route path={ROUTES.SIGNUP.replace('/', '')} element={<SignupScreen />} />
+        <Route path={ROUTES.FORGOT_PASSWORD.replace('/', '')} element={<ForgotPasswordScreen />} />
+        <Route path={ROUTES.RESET_PASSWORD.replace('/', '')} element={<ResetPasswordScreen />} />
+        {/* Default route redirects to welcome screen */}
+        <Route path="*" element={<WelcomeScreen />} />
       </Routes>
     </div>
   );
